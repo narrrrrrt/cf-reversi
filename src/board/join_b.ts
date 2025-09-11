dd262b9f0b727fa87953e2d2269fcc9dfc9f7e76
narrrrrrt <222976023+narrrrrrt@users.noreply.github.com>
2025-09-11 17:44:17 +0900

    0.2.4

--- src/board/join_b.ts
+++ src/board/join_b.ts
@@ -4,33 +4,25 @@ export function join(_: Room, token: string, seat: Seat): Seat {
   if (seat === "black" && !_.black) {
     _.black = token;
     _.step++;
-
-    // ★ 黒白両方揃ったら合法手を生成
-    if (_.black && _.white) {
-      _.status = "black"; // 先手は黒
-      _.boardData = _.legalBoard("black");
-    } else {
-      _.status = "waiting";
-      _.boardData = _.defaultBoard();
-    }
-    return "black";
-  }
-
-  if (seat === "white" && !_.white) {
+  } else if (seat === "white" && !_.white) {
     _.white = token;
     _.step++;
-
-    // ★ 黒白両方揃ったら合法手を生成
-    if (_.black && _.white) {
-      _.status = "black"; // 先手は黒
-      _.boardData = _.legalBoard("black");
-    } else {
-      _.status = "waiting";
-      _.boardData = _.defaultBoard();
-    }
-    return "white";
+  } else if (seat === "observer") {
+    _.observers.push(token);
+    return "observer";
+  } else {
+    // すでに埋まっている席に join しようとした場合
+    return seat;
   }
 
-  _.observers.push(token);
-  return "observer";
+  // ★ 黒白両方揃ったら黒ターンで開始
+  if (_.black && _.white) {
+    _.status = "black";
+    _.boardData = _.legalBoard("black");
+  } else {
+    _.status = "waiting";
+    _.boardData = _.defaultBoard();
+  }
+
+  return seat;
 }
